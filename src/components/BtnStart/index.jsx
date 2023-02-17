3d67e910675ff34452ad012664203b0f0c35a830
Claretta <116550253+Klairammm@users.noreply.github.com>
2023-02-17 16:15:26 +0100

    feat:add-start (#18)

---
+++ src/components/BtnStart/index.jsx
@@ -0,0 +1,34 @@
+import style from "./index.module.scss";
+import imgStart from "./start.png";
+import Image from "next/image";
+
+const Start = () => {
+  return (
+    <div className={style.Start}>
+      <button className={style.btn}>
+        <Image src={imgStart} width={300} height={300} />
+      </button>
+      <div className={style.controls}>
+        <div className={style.dpad}>
+          <div className={style.up}></div>
+          <div className={style.side}>
+            <div className={style.left}></div>
+            <div className={style.right}></div>
+          </div>
+          <div className={style.down}></div>
+        </div>
+        <div className={style.ab}>
+          <div className={style.btnB}>
+            <div className={style.b}>B</div>
+          </div>
+
+          <div className={style.btnA}>
+            <div className={style.a}>A</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
