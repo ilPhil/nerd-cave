0dbb185f476280fafb050247ac652a44991dbb57
sicknuccio <giardino495@gmail.com>
2023-02-17 12:51:14 +0100

    feat: add route

--- src/components/TitleBar/index.jsx
+++ src/components/TitleBar/index.jsx
@@ -1,26 +1,25 @@
 import styles from "./index.module.scss";
 
-const TitleBar = () => {
-    return (
-        <div className={styles.TitleBar}>
-            <div>
-               <div className={styles.contain}>
-                 <img className={styles.logo} src="" alt="logo"></img>
-                 <div className={styles.description}>
-                   <div className={styles.text}>
-                    <h2>Film/telefilm</h2>
-                    <input placeholder="cerca..."></input>
-                   </div>
-                   <div className={styles.buttons}>
-                    <button>I tuoi film</button>
-                    <button>Lista</button>
-                   </div>
-                 </div>
-               </div>
-              
+const TitleBar = ({ title }) => {
+  return (
+    <div className={styles.TitleBar}>
+      <div>
+        <div className={styles.contain}>
+          <img className={styles.logo} src="" alt="logo"></img>
+          <div className={styles.description}>
+            <div className={styles.text}>
+              <h2>{title}</h2>
+              <input placeholder="cerca..."></input>
             </div>
+            <div className={styles.buttons}>
+              <button>I tuoi film</button>
+              <button>Lista</button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default TitleBar;
