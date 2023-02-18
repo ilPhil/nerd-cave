0cd23a4128b159e1305fbba25a963ed990f9809c
ElisaAzzolina <creative.azzolina@gmail.com>
2023-02-18 17:33:09 +0100

    fix:css

--- src/components/singleMessageSidebar/Index.jsx
+++ src/components/singleMessageSidebar/Index.jsx
@@ -3,8 +3,14 @@ import styles from "./index.module.scss";
 function SingleMessageSidebar({ data }) {
   return (
     <div className={styles.SingleMessageSidebar}>
-      <h3>{data.name}</h3>
-      <p>{data.text}</p>
+      <img src={data.avatar} alt="avatar" />
+      <div className={styles.text}>
+        <div className={styles.nameTime}>
+          <h3>{data.name}</h3>
+          <p>09:23</p>
+        </div>
+        <p>{data.text}</p>
+      </div>
     </div>
   );
 }
