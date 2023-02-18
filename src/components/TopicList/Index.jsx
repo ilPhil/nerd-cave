6b566707476dcdcd256c5cbb5a290a7c09ebbda9
ElisaAzzolina <116554210+ElisaAzzolina@users.noreply.github.com>
2023-02-18 17:34:14 +0100

    fix:css (#31)

--- src/components/TopicList/Index.jsx
+++ src/components/TopicList/Index.jsx
@@ -1,22 +1,12 @@
 import styles from "./index.module.scss";
-
-import { BsFillPlayCircleFill } from "react-icons/bs";
-
 import React from "react";
-
 import SingleTopic from "../SingleTopic/Index";
-
 import { global_chats } from "@/utils/constants";
 
 function TopicList() {
   return (
     <div className={styles.topics}>
-      <h3>
-        <span>
-          <BsFillPlayCircleFill />
-        </span>
-        Canali:
-      </h3>
+      <h3>Canali:</h3>
       <ul>
         {global_chats.map((topic) => (
           <SingleTopic data={topic} key={topic.id} />
--- src/components/chatSidebar/Index.jsx
+++ src/components/chatSidebar/Index.jsx
@@ -1,37 +1,33 @@
 import styles from "./index.module.scss";
-
 import logo from "../../../public/LogoNerdCave.png";
-import { AiFillHome } from "react-icons/ai";
-import { BsFillPlayCircleFill } from "react-icons/bs";
-
 import SingleMessageSidebar from "../singleMessageSidebar/Index";
 import TopicList from "../TopicList/Index";
 import Image from "next/image";
 
 const mockChat = [
   {
-    avatar: "1.jpg",
+    avatar: "https://picsum.photos/60/60?1",
     createdAt: "1-2-3",
     name: "Nuccio",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus augue sapien, commodo vitae dignissim id",
+    text: "Lorem ipsum ",
     uid: "1",
   },
   {
-    avatar: "2.jpg",
+    avatar: "https://picsum.photos/60/60?2",
     createdAt: "1-2-3",
     name: "Tiziano",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus augue sapien",
     uid: "2",
   },
   {
-    avatar: "3.jpg",
+    avatar: "https://picsum.photos/60/60?3",
     createdAt: "1-2-4",
     name: "Elisa",
     text: "Mauris aliquet gravida tempor. Pellentesque pretium nisi lacus, at dignissim tortor bibendum sed. In hac habitasse platea dictumst. Nulla lacus neque, sollicitudin tristique justo vitae, ullamcorper euismod neque. Morbi scelerisque tortor at facilisis rutrum. Sed at feugiat arcu, vel eleifend turpis. Proin egestas placerat nulla blandit aliquam.",
     uid: "3",
   },
   {
-    avatar: "4.jpg",
+    avatar: "https://picsum.photos/60/60?4",
     createdAt: "1-2-4",
     name: "Jennifer",
     text: "Mauris aliquet gravida tempor. Pellentesque pretium nisi lacus, at dignissim tortor bibendum sed. In hac habitasse platea dictumst.",
@@ -43,20 +39,14 @@ function ChatSidebar() {
   return (
     <div className={styles.ChatSidebar}>
       <div className={styles.home}>
-        <Image src={logo} alt="logo" width={100} height={100}/>
+        <Image src={logo} alt="logo" width={150} height={150} />
       </div>
-      <h3>
-        <span>
-          <BsFillPlayCircleFill />
-        </span>
-        Ultimi Messaggi:
-      </h3>
+      <h3 className={styles.title}>Ultimi Messaggi:</h3>
       <div className={styles.lastMsgs}>
         {mockChat.map((item) => (
           <SingleMessageSidebar data={item} key={item.uid} />
         ))}
       </div>
-      <hr></hr>
       <div className={styles.topics}>
         <TopicList />
       </div>
@@ -66,7 +56,10 @@ function ChatSidebar() {
           src="https://picsum.photos/60/60"
           alt="user"
         />
-        <p>Nome Utente</p>
+        <div className={styles.userText}>
+          <p>Nome Utente</p>
+          <button>Logout</button>
+        </div>
       </div>
     </div>
   );
