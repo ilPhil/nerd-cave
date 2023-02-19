3d480d2f0b769b6ac39189a7a4a18f56cb4454ec
Tirzo01 <tiziano.nicosia01@gmail.com>
2023-02-19 12:49:42 +0100

    fix: fixed routes in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,27 +5,27 @@ import { FaPen, FaBook } from "react-icons/fa";
 export const global_chats = [
   {
     name: "Film/Telefilm",
-    route: "/film-telefilm",
+    route: "/chat/film-telefilm",
     icon: <BsFilm />,
   },
   {
     name: "Gaming",
-    route: "/gaming",
+    route: "/chat/gaming",
     icon: <SiYoutubegaming />,
   },
   {
     name: "Musica",
-    route: "/musica",
+    route: "/chat/musica",
     icon: <SiYoutubemusic />,
   },
   {
     name: "Manga/Anime",
-    route: "/manga-anime",
+    route: "/chat/manga-anime",
     icon: <FaPen />,
   },
   {
     name: "Letteratura",
-    route: "/letteratura",
+    route: "/chat/letteratura",
     icon: <FaBook />,
   },
 ];
