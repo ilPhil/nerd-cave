f2d5d40416dd4ac944b386ccbb36be8f2f01a88d
Claretta <116550253+Klairammm@users.noreply.github.com>
2023-02-19 21:42:59 +0100

    fix:cssMessagesPages (#43)

---
+++ public/images.js
@@ -0,0 +1,9 @@
+import letteratura from "./letteratura.png";
+import messages from "./messages.png";
+
+const imagesChat = {
+  letteratura: letteratura,
+  messages: messages,
+};
+
+export default imagesChat;
