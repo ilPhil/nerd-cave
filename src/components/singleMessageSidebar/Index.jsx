1ef9640f149d0fd6e0215243f2837b265658389f
ElisaAzzolina <116554210+ElisaAzzolina@users.noreply.github.com>
2023-02-19 11:13:56 +0100

    fix:singleMsgSidebar bug (#34)

--- src/components/singleMessageSidebar/Index.jsx
+++ src/components/singleMessageSidebar/Index.jsx
@@ -2,23 +2,23 @@ import styles from "./index.module.scss";
 
 function SingleMessageSidebar({ data }) {
   console.log(data.otherUser);
-  console.log("Immagine: ", data.otherUser.photoURL);
+  console.log("Immagine: ", data?.otherUser?.photoURL);
   return (
     <div className={styles.SingleMessageSidebar}>
-      <img src={data.otherUser.photoURL} alt="avatar" />
+      <img src={data?.otherUser?.photoURL} alt="avatar" />
       <div className={styles.text}>
         <div className={styles.nameTime}>
-          <h3>{data.otherUser.displayName}</h3>
-          <p>{convertTimeStamp(data.lastMessage.createdAt)}</p>
+          <h3>{data?.otherUser?.displayName}</h3>
+          <p>{convertTimeStamp(data?.lastMessage?.createdAt)}</p>
         </div>
-        <p>{data.lastMessage.text}</p>
+        <p>{data?.lastMessage?.text}</p>
       </div>
     </div>
   );
 }
 
 const convertTimeStamp = (timestamp) => {
-  const date = new Date(timestamp.seconds * 1000);
+  const date = new Date(timestamp?.seconds * 1000);
   return date.toLocaleTimeString(navigator.language, {
     hour: "2-digit",
     minute: "2-digit",
