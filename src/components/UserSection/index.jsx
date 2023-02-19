e367338b460b0c91fb254d29aa4c36435146c810
Tirzo01 <tiziano.nicosia01@gmail.com>
2023-02-19 12:14:26 +0100

    feat: Added UserSection componenents with logic

---
+++ src/components/UserSection/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./index.module.scss";
+
+import { auth, db } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export const UserSection = () => {
+  const [user] = useAuthState(auth);
+  return (
+    <div className={styles.user}>
+      <img className={styles.userImg} src={user.photoURL} alt="user" />
+      <div className={styles.userText}>
+        <p>{user.displayName}</p>
+        <button ocClick={() => auth.signOut()}>Logout</button>
+      </div>
+    </div>
+  );
+};
