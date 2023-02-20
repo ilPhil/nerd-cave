363cff74dee264cf0dd1a565bcd46b91df525048
ElisaAzzolina <creative.azzolina@gmail.com>
2023-02-20 17:18:21 +0100

    fix:background-css

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,14 @@
 import { BsFilm } from "react-icons/bs";
 import { SiYoutubegaming, SiYoutubemusic } from "react-icons/si";
 import { FaPen, FaBook } from "react-icons/fa";
+import { TfiWorld } from "react-icons/tfi";
 
 export const global_chats = [
+  {
+    name: "Globale",
+    route: "/chat/messages",
+    icon: <TfiWorld />,
+  },
   {
     name: "Film/Telefilm",
     route: "/chat/film-telefilm",
