60f2c7e262d4a3db867c4dfb5e298b5a47f0c666
ElisaAzzolina <116554210+ElisaAzzolina@users.noreply.github.com>
2023-02-21 15:17:15 +0100

    fix:link (#57)

--- src/components/Modal/Index.jsx
+++ src/components/Modal/Index.jsx
@@ -1,23 +1,27 @@
+import SearchUser from "../SearchUser";
 import styles from "./index.module.scss";
 
-const Modal = () => {
-    return (
-        <div className={styles.overlay}>
-         <div className={styles.Modal}>
-            <input placeholder="Cerca..."></input>
-            <div className={styles.utentList}>
-                <div className={styles.utent}>
-                  <img src="https://picsum.photos/50/50?1" alt="logo_utente"></img>
-                  <h3>Nome utente 1</h3>
-                </div>
-                <div className={styles.utent}>
-                  <img src="https://picsum.photos/50/50?2" alt="logo_utente"></img>
-                  <h3>Nome utente 2</h3>
-                </div>
-            </div>
-         </div>
+const Modal = ({ setModalEnabled }) => {
+  const onHandleModal = () => setModalEnabled(false);
+
+  return (
+    <div className={styles.overlay}>
+      <button onClick={onHandleModal}>X</button>
+      <div className={styles.Modal}>
+        <SearchUser />
+        <div className={styles.utentList}>
+          <div className={styles.utent}>
+            <img src="https://picsum.photos/50/50?1" alt="logo_utente"></img>
+            <h3>Nome utente 1</h3>
+          </div>
+          <div className={styles.utent}>
+            <img src="https://picsum.photos/50/50?2" alt="logo_utente"></img>
+            <h3>Nome utente 2</h3>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
