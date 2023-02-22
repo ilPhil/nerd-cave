91e23558015b43efae5cb924ba2c07e0e01d7a27
Klairammm <mclarettaf@gmail.com>
2023-02-22 12:47:13 +0100

    fix:menuHamburger

--- src/components/Hamburger/Index.jsx
+++ src/components/Hamburger/Index.jsx
@@ -1,69 +1,49 @@
 import style from "./style.module.scss";
 
-import { BsFillChatDotsFill, BsFilm } from "react-icons/bs";
-import { SiYoutubegaming, SiYoutubemusic } from "react-icons/si";
-import { FaPen, FaBook } from "react-icons/fa";
 import { global_chats } from "@/utils/constants";
 import { UserSection } from "../UserSection";
+import Link from "next/link";
+import Modal from "../Modal/Index";
+import { useState } from "react";
+import { MdMessage } from "react-icons/md";
 
-const Hamburger = () => {
+const Hamburger = ({ setNode }) => {
+  const [isModalEnabled, setModalEnabled] = useState(false);
+  const onHandleModal = () => setModalEnabled((prev) => !prev);
   return (
-    <section className={style.menu}>
-      <div className={style.hamburgerMenu}>
-        <input className={style.menu__toggle} type="checkbox" />
-        <label className={style.menu__btn} htmlFor="menu__toggle">
-          <span></span>
-        </label>
+    <>
+      <section className={style.menu}>
+        {isModalEnabled ? (
+          <Modal setNode={setNode} setModalEnabled={setModalEnabled} />
+        ) : (
+          <div className={style.hamburgerMenu}>
+            <input className={style.menu__toggle} type="checkbox" />
+            <label className={style.menu__btn} htmlFor="menu__toggle">
+              <span></span>
+            </label>
 
-        <ul className={style.menu__box}>
-          {global_chats.map((topic) => (
-            <li className={style.li__item}>
-              {topic.icon}
-              <a className={style.menu__item} href={topic.route}>
-                {topic.name.replace("-", " e ")}
-              </a>
-            </li>
-          ))}
-          {/*  <li className={style.li__item}>
-            <BsFillChatDotsFill />
-            <a className={style.menu__item} href="https://www.google.com">
-              Chat
-            </a>
-          </li>
-          <li className={style.li__item}>
-            <BsFilm />
-            <a className={style.menu__item} href="https://www.google.com">
-              Film/Telefilm
-            </a>
-          </li>
-          <li className={style.li__item}>
-            <SiYoutubemusic />
-            <a className={style.menu__item} href="https://www.google.com">
-              Musica
-            </a>
-          </li>
+            <ul className={style.menu__box}>
+              <li className={style.logout}>
+                <UserSection />
+              </li>
+              {global_chats.map((topic) => (
+                <li className={style.li__item}>
+                  {topic.icon}
+                  <Link className={style.menu__item} href={topic.route}>
+                    {topic.name.replace("-", " e ")}
+                  </Link>
+                </li>
+              ))}
 
-          <li className={style.li__item}>
-            <SiYoutubegaming />
-            <a className={style.menu__item} href="https://www.google.com">
-              Gaming
-            </a>
-          </li>
-          <li className={style.li__item}>
-            <FaPen />
-            <a className={style.menu__item} href="https://www.google.com">
-              Anime/Manga
-            </a>
-          </li>
-          <li className={style.li__item}>
-            <FaBook />
-            <a className={style.menu__item} href="https://www.google.com">
-              Letteratura
-            </a>
-          </li> */}
-        </ul>
-      </div>
-    </section>
+              <li className={style.privateChat} onClick={onHandleModal}>
+                <MdMessage />
+                Inizia una chat privata
+              </li>
+            </ul>
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
